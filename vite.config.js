af8f11aae0a4edf28c360d29eb28431dff3bd41c
Manjur-WD <114661279+Manjur-WD@users.noreply.github.com>
2024-11-20 16:22:40 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base:"/ecommWebapp/",
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',  // Automatically update the service worker
+      devOptions: {
+        enabled: true,  // Enable during development for easier testing
+      },
+      manifest: {
+        name: 'EcommWebApp Lite',
+        short_name: 'Ecommerce',
+        description: 'Ecommerce Platform',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone', // App behaves like a native app
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
